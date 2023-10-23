24ea14d15b3e97f8a70bbdc60e946be8fcc2204a
joaocarlos396 <2022172@student.cct.ie>
2023-10-23 21:21:10 +0100

    Initial commit

---
+++ src/person/Person.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package person;
+
+/**
+ *
+ * @author joao
+ */
+public class Person {
+    private int number;
+    private String name;
+    private String LastName;
+    private String email;
+    
+    public Person(int number, String name, String LastName, String email){
+        this.number=number;
+        this.name=name;
+        this.LastName=LastName;
+        this.email=email;
+    }
+} 
